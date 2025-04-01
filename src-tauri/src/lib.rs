b5d8e6b56dbc513f4f99ac3ad356e4e03e9e1acf
Harish Bhawnani <harishmmp@gmail.com>
2025-04-01 16:08:04 +0530

    updated dependencies

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,8 +9,6 @@ pub mod utils;
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![generate_result])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
